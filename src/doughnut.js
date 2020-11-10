67fe402014a18ae56d8428025fe963f5634b71fd
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-11-10 09:37:33 +0500

    doughnut for toxin buildDoughnut with calculating % -> rads

---
+++ src/doughnut.js
@@ -0,0 +1,46 @@
+function init(data) {
+    var canvas = document.getElementById("test");
+        canvas.width = 120;
+        canvas.height = 120;
+
+    var context = canvas.getContext("2d");
+        context.beginPath();
+        context.strokeRect(0, 0, canvas.width, canvas.height);
+        context.stroke();
+
+
+    var summary = 0;
+        angle = [];
+        position = [-0.5*Math.PI];
+
+        colors = ["black", "#6FCF97", "#BC9CFF", "red"];
+
+    function buildDoughnut(data) {
+        summ = (x, y) => x + y;
+        summary = data.reduce(summ);
+        data.forEach(function(item, i, data) {
+            angle[i] = item/summary*100*0.062831853071796;
+        })
+        
+        for (let i = 1; i < angle.length; i++) {
+            position[i] = position[i-1] - angle[i-1];
+        }
+
+        context.lineWidth = 4;
+        position[4] = (-0.5*Math.PI);
+        for (let i = 0; i < data.length; i++) {
+            context.beginPath();
+            context.strokeStyle = colors[i];
+            if (data[i] == 0) {
+                context.arc(60, 60, 58, position[i], position[i], true);
+            } else {
+                context.arc(60, 60, 58, position[i], position[i+1] + 0.0174533, true);
+            }
+            context.stroke();
+        }
+
+
+    }
+
+    buildDoughnut(data);
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import '../node_modules/jquery/dist/jquery';
+let $ = require('jquery');
+
+import './doughnut'
+
 // Autoadd all scss/css/ts files
 function importAll (r) {
     r.keys().forEach(r);
 }
   
-importAll(require.context('./', true, /\.scss|css|ts$/));
+importAll(require.context('./', true, /\.scss|css|js$/));
